d6c2bf115e85276b0da69fa3f7592c2bb5de855f
github username <github email address>
2020-01-13 16:31:23 -0500

    Done.

--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,3 +1,52 @@
 class Startup
+    attr_reader :name, :founder, :domain
+    @@all = []
 
+    def initialize(name, founder, domain)
+        @name = name
+        @founder = founder
+        @domain = domain
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def pivot(domain, name)
+        @domain = domain
+        @name = name
+    end
+
+    def self.find_by_founder(name)
+        all.find{|startup| startup.name == name}
+    end
+
+    def self.domains
+        all.map{|startup| startup.domain}
+    end
+
+    def sign_contract(venture_capitalist, type, investment)
+        FundingRound.new(self, venture_capitalist, type, investment)
+    end
+
+    def funding_rounds
+        FundingRound.all.select{|round| round.startup == self}
+    end
+
+    def num_funding_rounds
+        self.funding_rounds.size
+    end
+
+    def total_funds
+        self.funding_rounds.map{|round| round.investment}.sum
+    end
+
+    def investors
+        self.funding_rounds.map{|round| round.venture_capitalist}.uniq
+    end
+
+    def big_investors
+        self.investors & VentureCapitalist.tres_commas_club
+    end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,16 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+windows = Startup.new("Windows", "Gates", "Tech")
+apple = Startup.new("Apple", "Jobs", "Tech")
+rawr = Startup.new("RAWR", "Hunger Man", "Food")
+investor1 = VentureCapitalist.new("investor1", 5968765432)
+investor2 = VentureCapitalist.new("investor2", 3968765432)
+investor3 = VentureCapitalist.new("investor3", 968765432)
+investor4 = VentureCapitalist.new("investor4", 968765432)
+investor1.offer_contract(windows, "cause", 5000000)
+investor2.offer_contract(windows, "cause", 5400000)
+investor1.offer_contract(apple, "cause", 900000)
+rawr.sign_contract(investor3, "cause", 5)
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
